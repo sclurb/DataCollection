d6643d63b7264a22d9db6f433cc1cd7733099a76
Robert Donovan <sclurb@outlook.com>
2018-03-06 16:23:46 -0500

    Did a lot of fooling around wih the installer calss and a few helper classes

---
+++ DataCollectionCustomInstaller/SqlProbe.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.SqlServer.Management.Smo;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLTest
+{
+    public class SqlProbe
+    {
+
+        private string p = @"Bobzilla_16\SqlExpress";
+        private string standard = "Microsoft SQL Server 2012";
+
+        public SqlProbe()
+        {
+
+        }
+
+        public string Select(List<string> str)
+        {
+            string p = null;
+            foreach(string a in str)
+            {
+                if (Sql(a))
+                {
+                    p = a;
+                }
+            }
+
+            if (p != null)
+            {
+                return p;
+            }
+            else
+            {
+                return "No Match!";
+            }
+            
+        }
+
+        public bool Sql(string server)
+        {
+            try
+            {
+                DataTable result = new DataTable();
+                string connectionString = String.Format("Server={0}; Database=; Integrated Security=True;", server);
+                string s = " ";
+                string nonQuery = "Select @@version";
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(nonQuery, conn))
+                    {
+                        SqlDataReader rdr = cmd.ExecuteReader();
+                        result.Load(rdr);
+                    }
+                    s = result.Rows[0]["Column1"].ToString();
+                }
+                s = Truncate(s, 25);
+                if (s.Equals(standard))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (SqlException e)
+            {
+                MessageBox.Show(e.ToString());
+                return false;
+            }
+        }
+
+        public string Truncate(string s, int maxLength)
+        {
+            return s != null && s.Length > maxLength ? s.Substring(0, maxLength) : s;
+        }
+
+
+        public List<string> gather()
+        {
+            List<string> floyd = new List<string>();
+            DataTable dt = SmoApplication.EnumAvailableSqlServers(true);
+
+            foreach (DataRow a in dt.Rows)
+            {
+                floyd.Add(a[0].ToString());
+            }
+            return floyd;
+        }
+
+        public List<string> Go()
+        {
+            List<string> goober = new List<string>();
+            RegistryView registryView = Environment.Is64BitOperatingSystem ? RegistryView.Registry64 : RegistryView.Registry32;
+            using (RegistryKey hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, registryView))
+            {
+                RegistryKey instanceKey = hklm.OpenSubKey(@"SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL", false);
+                if (instanceKey != null)
+                {
+                    foreach (var instanceName in instanceKey.GetValueNames())
+                    {
+                        goober.Add(Environment.MachineName + "\\" + instanceName);
+                    }
+                }
+            }
+            return goober;
+        }
+    }
+}
